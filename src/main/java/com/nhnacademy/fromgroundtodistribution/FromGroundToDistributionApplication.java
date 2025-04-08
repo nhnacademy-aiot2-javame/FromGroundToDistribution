2b243328b384df2ba204cbe9ed65614b957cae19
InSeong23 <seungin2360@gmail.com>
2025-04-08 17:56:02 +0900

    init

---
+++ src/main/java/com/nhnacademy/fromgroundtodistribution/FromGroundToDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.fromgroundtodistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromGroundToDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromGroundToDistributionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/nhnacademy/fromgroundtodistribution/index/IndexController.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.fromgroundtodistribution.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping(value = {"/index.html" , "/"})
+    public String index() {return "index/index";}
+}
